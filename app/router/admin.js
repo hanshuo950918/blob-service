889139bb79d61165057bfe6d5caa5d81aec81886
hanshuo0918 <905068260@qq.com>
2020-12-22 23:07:26 +0800

    '二次提交'

--- app/router/admin.js
+++ app/router/admin.js
@@ -1,5 +1,11 @@
 module.exports = app => {
   const { router, controller } = app
-  router.get('/admin/index', controller.admin.main.index)
-  router.post('/admin/checkOpenId',controller.admin.main.checkLogin)
+  const adminauth = app.middleware.adminauth();//中间件类似路由守卫
+  router.post('/admin/checkLogin', controller.admin.main.checkLogin)
+  router.get('/admin/getTypeInfo', adminauth, controller.admin.main.getTypeInfo)
+  router.post('/admin/addArticle', adminauth, controller.admin.main.addArticle)
+  router.post('/admin/updateArticle', adminauth, controller.admin.main.updateArticle)
+  router.get('/admin/getArticleList', adminauth, controller.admin.main.getArticleList)
+  router.get('/admin/delArticle/:id',adminauth,controller.admin.main.delArticle)
+  router.get('/admin/getArticleById/:id',adminauth,controller.admin.main.getArticleById)
 }
--- config/config.default.js
+++ config/config.default.js
@@ -48,8 +48,8 @@ module.exports = appInfo => {
     domainWhiteList: ['*']
   };
   config.cors = {
-    origin: 'http://localhost:3000/', //只允许这个域进行访问接口
-    credentials: true,   // 开启认证
+    credentials: true,   // 允许cookie跨域
+    origin: 'http://localhost:3000', //只允许这个域进行访问接口
     allowMethods: 'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS'
   };
 
